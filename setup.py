a591e4158185e34e3401134398cfc2102575cbd8
Pierre Rouanet <pierre.rouanet@gmail.com>
2018-10-09 15:26:15 +0200

    Setup project.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import imp
+
+from setuptools import setup, find_packages
+
+version = imp.load_source('jellylamp.version', 'jellylamp/version.py')
+
+setup(
+    name='jellylamp',
+    version=version.version,
+    packages=find_packages(),
+
+    install_requires=[
+    ],
+
+    extra_require={
+        'tests': [],
+    },
+
+    author='Pollen Robotics',
+    author_email='contact@pollen-robotics.com',
+    description='Python library for controlling the Jelly fish lamp.',
+    url='https://github.com/pollen-robotics/jellylamp',
+    license='Apache License Version 2.0',
+)
