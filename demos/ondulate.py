58821b9ff9e1d0cf7924cbc9cfa437a68885ff11
Pierre Rouanet <pierre.rouanet@gmail.com>
2018-10-10 19:22:28 +0200

    Update demo.

--- demos/ondulate.py
+++ demos/ondulate.py
@@ -1,21 +1,45 @@
-import math
+import numpy as np
 import time
 
 from jellylamp import JellyLamp
 
+colors = 'off red green yellow blue pink cyan white'.split(' ')
+
+
 if __name__ == '__main__':
     lamp = JellyLamp(
         ids_groups=(
-            (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),
-            (13, 14, 15, 16, 17, 18, 19, 20, 21, 22),
-            (23, 24, 25, 26, 27, 28, 29, 30, 31, 32),
+            (1, 2, 3, 4, 5, 6, 7, 8),
+            (9, 10, 11, 12, 13, 14, 15, 16),
         ),
         sync_freq=100.0,
+        offsets={},
     )
 
-    t0 = time.time()
-    while time.time() - t0 < 30:
-        p = 30 * math.sin(2 * math.pi * 0.25 * time.time())
-        for m in lamp.motors:
-            m.goal_position = p
-        time.sleep(.01)
+    up_motors = [m for m in lamp.motors if m.id % 2 == 0]
+    low_motors = [m for m in lamp.motors if m.id % 2 == 1]
+
+    for m in up_motors:
+        m.goal_position = 100
+
+    for m in low_motors:
+        m.goal_position = -125
+
+    time.sleep(1)
+
+    up_center, up_amp, up_freq = 50, 50, 0.25
+    low_center, low_amp, low_freq = -25, 100, 0.25
+
+    while True:
+        up_pos = up_center + up_amp * np.sin(2 * np.pi * up_freq * time.time())
+        low_pos = low_center + low_amp * np.sin(2 * np.pi * low_freq * (time.time() + 2))
+
+        for i, m in enumerate(up_motors):
+            # up_pos = up_center + up_amp * np.sin(2 * np.pi * up_freq * (time.time() + i / 2))
+            m.goal_position = up_pos
+
+        for i, m in enumerate(low_motors):
+            # low_pos = low_center + low_amp * np.sin(2 * np.pi * low_freq * (time.time() + i / 2))
+            m.goal_position = low_pos
+
+        time.sleep(0.01)
