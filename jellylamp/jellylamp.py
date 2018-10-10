1f8f9ac69de1c1f0216abfa536c1080c6014d5a9
Pierre Rouanet <pierre.rouanet@gmail.com>
2018-10-10 12:16:57 +0200

    Add support for led color control.

--- jellylamp/jellylamp.py
+++ jellylamp/jellylamp.py
@@ -8,7 +8,8 @@ from .motor import Motor
 class JellyLamp(object):
     def __init__(self, ids_groups, sync_freq):
         self._ios = auto_open_ios(ids_groups)
-        self._pos = {}
+
+        self._pos, self._color = {}, {}
 
         self.motors = []
         for ids, io in self._ios.items():
@@ -25,6 +26,8 @@ class JellyLamp(object):
     def _sync(self):
         while True:
             self._push_pos()
+            self._push_color()
+
             time.sleep(self.sync_period)
 
     def _push_pos(self):
@@ -38,3 +41,15 @@ class JellyLamp(object):
                 return
             io.set_goal_position(pos_for_ids)
         self._pos = {}
+
+    def _push_color(self):
+        for ids, io in self._ios.items():
+            color_for_ids = {
+                id: self._color[id]
+                for id in ids
+                if id in self._color
+            }
+            if not color_for_ids:
+                return
+            io.set_LED_color(color_for_ids)
+        self._color = {}
--- jellylamp/motor.py
+++ jellylamp/motor.py
@@ -1,8 +1,12 @@
 class Motor(object):
+    colors = 'off red green yellow blue pink cyan white'.split(' ')
+
     def __init__(self, id, delegate):
         self.id = id
         self.delegate = delegate
+
         self._goal_pos = None
+        self._color = None
 
     @property
     def goal_position(self):
@@ -12,3 +16,15 @@ class Motor(object):
     def goal_position(self, new_pos):
         self._goal_pos = new_pos
         self.delegate._pos[self.id] = new_pos
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, new_color):
+        if new_color not in self.colors:
+            raise ValueError('color must be one of {}'.format(self.colors))
+
+        self._color = new_color
+        self.delegate._color[self.id] = new_color
