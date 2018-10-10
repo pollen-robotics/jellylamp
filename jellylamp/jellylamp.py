2606930e5641f6ad53ce7a0bfe4ab627444ac536
Pierre Rouanet <pierre.rouanet@gmail.com>
2018-10-10 12:26:08 +0200

    Abstract register writing.

--- jellylamp/jellylamp.py
+++ jellylamp/jellylamp.py
@@ -9,7 +9,7 @@ class JellyLamp(object):
     def __init__(self, ids_groups, sync_freq):
         self._ios = auto_open_ios(ids_groups)
 
-        self._pos, self._color = {}, {}
+        self._reg = {'pos': {}, 'color': {}}
 
         self.motors = []
         for ids, io in self._ios.items():
@@ -24,32 +24,26 @@ class JellyLamp(object):
         sync_t.start()
 
     def _sync(self):
+        sync_reg = (
+            ('pos', 'set_goal_position'),
+            ('color', 'set_LED_color'),
+        )
+
         while True:
-            self._push_pos()
-            self._push_color()
+            for (reg, setter) in sync_reg:
+                self._push_val(reg, setter)
 
             time.sleep(self.sync_period)
 
-    def _push_pos(self):
-        for ids, io in self._ios.items():
-            pos_for_ids = {
-                id: self._pos[id]
-                for id in ids
-                if id in self._pos
-            }
-            if not pos_for_ids:
-                return
-            io.set_goal_position(pos_for_ids)
-        self._pos = {}
+    def _push_val(self, reg, setter):
+        values = self._reg[reg]
 
-    def _push_color(self):
         for ids, io in self._ios.items():
-            color_for_ids = {
-                id: self._color[id]
-                for id in ids
-                if id in self._color
+            val_for_ids = {
+                id: values[id]
+                for id in ids if id in values
             }
-            if not color_for_ids:
-                return
-            io.set_LED_color(color_for_ids)
-        self._color = {}
+            if val_for_ids:
+                getattr(io, setter)(val_for_ids)
+
+        values.clear()
